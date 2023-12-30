032b00c524dba14ccf2da147a7efa2db7cd4697b
SsSseo <139518640+SsSseo@users.noreply.github.com>
2023-12-30 11:25:59 +0900

    [Silver IV] Title: Hexagonal Tiles, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/5705. Hexagonal Tiles/Hexagonal Tiles.py
+++ 백준/Silver/5705. Hexagonal Tiles/Hexagonal Tiles.py
@@ -1,23 +1,11 @@
-def count_sequences(n):
-    if n == 1 or n == 2:
-        return n
-
-    dp = [0] * (n + 1)
-    dp[1] = 1
-    dp[2] = 2
-
-    for i in range(3, n + 1):
-        dp[i] = dp[i - 1] + dp[i - 2]
-        # 이진트리적으로 봤을때 앞에 두값은 현재 i의 값과 연결되어있는 앞에 값이다
-        # 그래서 그 두값을 더하니 현재의 값까지의 이동 개수가 나왔다
-
-    return dp[n]
-
-
+import sys
 while True:
-    n = int(input())
-    if n == 0:
+    N = int(sys.stdin.readline())
+    if N == 0:
         break
-
-    result = count_sequences(n)
-    print(result)
+    arr = [0] * (N+1)
+    arr[0] = 1
+    arr[1] = 1
+    for i in range(2,N+1):
+        arr[i] = arr[i-1]+arr[i-2]
+    print(arr[N])
