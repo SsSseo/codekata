439ff71c3523573aab6d18edaedce9840f724748
SsSseo <139518640+SsSseo@users.noreply.github.com>
2023-12-30 11:25:48 +0900

    [Bronze I] Title: Hunt The Rabbit, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/13777. Hunt The Rabbit/Hunt The Rabbit.py
+++ 백준/Bronze/13777. Hunt The Rabbit/Hunt The Rabbit.py
@@ -1,22 +1,20 @@
+import sys
+
 while True:
-    start, end = 1, 50
-    N = int(input())
-    # N이 0이면 반복을 끝낸다
-    if N == 0:
+    ans = int(sys.stdin.readline())
+    if ans == 0:
         break
-    while True:
-    	# start와 end의 중간을 구한다
-        n = (start + end) // 2
-        # 중간을 프린트
-        print(n, end=' ')
-        # n과  N이 동일하지면 반복 끝낸다
-        if n == N:
+
+    l, r = 1,50
+
+    while l<=r:
+        mid = (l+r)//2
+        print(mid, end =" ")
+        if ans == mid:
             break
-        # 중간 수보다 작다면 end를 줄인다
-        elif n > N:
-            end = n - 1
-        # 중간수 보다 크다면 start를 늘린다
+
+        if ans > mid:
+            l = mid+1
         else:
-            start = n + 1
-    # 반복이 끝나면 출력 줄을 바꾼다.
-    print(sep = '\n')
+            r = mid -1
+    print("")
